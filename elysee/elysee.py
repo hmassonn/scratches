1362694e62e287815e7ef74f70c0afa20052e858
olag18 <hmassonn@e2r9p11.42.fr>
2019-05-14 18:04:37 +0200

    elysee

--- elysee/elysee.py
+++ elysee/elysee.py
@@ -2,24 +2,32 @@ from lxml import html
 import requests
 import csv
 
-first_page = requests.get('http://www.elysee.ch/expositions-et-evenements/expositions-dans-le-monde/precedentes')
-tree = html.fromstring(first_page.content)
+follow_page = ['http://www.elysee.ch/expositions-et-evenements/expositions-dans-le-monde/precedentes']
 
-follow_page = tree.xpath('//li[@class="tx-pagebrowse-next"]/a/attribute::href')
+two_p = ""
+pages = 0
 
-for one_url in pages_list:
+while pages < 62:
+    pages += 1
+    follow_page = requests.get(follow_page[0])
+    tree = html.fromstring(follow_page.content)
+    follow_page = tree.xpath('//li[@class="tx-pagebrowse-next"]/a/attribute::href')
+    link_list = tree.xpath("//div[@class='mel_module_liste']/div[@class='item']/a/attribute::href")
+
+    print(follow_page)
+
+    for one_url in link_list:
         one_page = requests.get(one_url)
         domo = html.fromstring(one_page.content)
-        content = domo.xpath("//div[@class='mel_module_liste']/div[@class='item']/a/attribute::href")
+        content = domo.xpath("//div[@class='mel_module_expo']//p[@class='bodytext']/text()")
 
-        file = open('beauxArts.txt', 'a')
-        two_p = ""
+        print(content)
+        file = open('elysee.txt', 'a')
         for one_p in content:
-                two_p += one_p
-                print(two_p)
-        file.write(two_p)
+            two_p += one_p
 
 
+file.write(two_p)
 
 
 
